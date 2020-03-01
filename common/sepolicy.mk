LineageOS/android_device_lineage_sepolicy

sepolicy: Initial repo structure

common/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/common

linerecov/android_device_carbon_sepolicy

common/sepolicy.mk
@@ -1,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/aquarios/sepolicy/common